10f2cf4a9d790b5799628233bc77ff196aa02cff
Vieira <caio2011dhd@gmail.com>
2024-04-21 11:16:29 -0300

    ajuste cupom

--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -4,9 +4,11 @@ export interface Props {
 }
 
 export default function({code, description}: Props){
-    return (<div class="container bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
-    <p class="block font-bold text-lg">{code}</p>
-    <br />
-    {description}
-</div>))
+    return (
+        <div class="container mb-2 bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
+            <p class="block font-bold text-lg">{code}</p>
+            <br />
+            {description}
+        </div>
+    )
 }
